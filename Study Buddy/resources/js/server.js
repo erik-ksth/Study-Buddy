b09785c749767d3544e877e45e81a2337c67d248
Kaung124 <74672970+Kaung124@users.noreply.github.com>
2021-10-26 23:48:51 +0630

    Update server.js

--- Study Buddy/resources/js/server.js
+++ Study Buddy/resources/js/server.js
@@ -196,12 +196,17 @@ const musics = [
         label : "lo-fi",
         dir : "lofi",
         playlist : ["Still Not Rite - Noir Et Blanc Vie",
+                    "7PL7ADKDYcw_bestaudio_mp3_0_180.1_637708608204837878",
                     "Knowpe - Noir Et Blanc Vie",
+                    "_Royalty Free Music 2am - Relaxing Dreamy Lo-Fi Music_160k",
                     "Midnight-Stroll-Lofi-Study-Music",
+                    "sunrise",
                     "Morning-Routine-Lofi-Study-Music", 
                     "barradeen-bedtime-after-a-coffee",
                     "On-My-Way-Lofi-Study-Music",
+                    "cold",
                     "purrple-cat-dreams-come-true",
+                    "_Royalty Free Music Rainy Thoughts - Relaxing Lofi Hip Hop Music_160k",
                     "Still-Awake-Lofi-Study-Music",
                     "2019-06-12_-_Homework_-_David_Fesliyan"],
     },
@@ -211,12 +216,25 @@ const musics = [
         icon : "resources/img/icons/jazz.svg",
         label : "jazz",
         dir: "jazz",
-        playlist : ["Casa-Bossa-Nova",
+        playlist : ["A Night Alone",
+                    "Almost A Year Ago",
+                    "Bet On It",
+                    "Bluesy Vibes",
+                    "Book Bag",
+                    "Casa-Bossa-Nova",
                     "CloserToJazz",
-                    "SidewaysSamba",
-                    "StandardJazzBars",
                     "Cocktail Hour - Aaron Kenny",
-                    "Lukewarm Hazy - Asher Fulero"]
+                    "Ersatz Bossa",
+                    "George Street Shuffle",
+                    "Home for the Holidays",
+                    "Just As Soon",
+                    "Long Stroll",
+                    "Lukewarm Hazy - Asher Fulero",
+                    "Nighttime Stroll",
+                    "SidewaysSamba",
+                    "Soul and Mind",
+                    "StandardJazzBars",
+                    "The Night Falling"]
     },
 
     {
@@ -224,26 +242,33 @@ const musics = [
         icon : "resources/img/icons/piano.svg",
         label : "piano",
         dir: "piano",
-        playlist : ["Allégro - Emmit Fenn",
+        playlist : ["Dreamland - Aakash Gandhi",
                     "Chords of Harmony - Aakash Gandhi",
-                    "Dreamland - Aakash Gandhi",
+                    "Allégro - Emmit Fenn",
                     "Invisible Beauty - Aakash Gandhi",
                     "Just Stay - Aakash Gandhi",
                     "Lullaby - JVNA",
-                    "No.4 Piano Journey - Esther Abrami",
                     "Til Death Parts Us - Aakash Gandhi",
+                    "Classical Sad Piano No Copyright Sound FREE USE MUSIC - Scott Buckley - Undertow_160k",
                     "True Art Real Affection Part 2 - Noir Et Blanc Vie"]
     },
 
     {
         id : 4,
-        icon : "resources/img/icons/kalimba.svg",
-        label : "kalimba",
-        dir: "kalimba",
-        playlist : ["Oriente - Kalimba Lullaby  Background Music-xXe10UJvqKU-192k-1635171501921",
-                    "Honolulu - Funny Upbeat Guitar & Kalimba Background Music-ubI_WdNH6DI-192k-1633838417110",
-                    "Kalimba Lullaby  Meditation  Relaxation-QLGvrULY5oA-192k-1635172123551",
-                    "Sleepy Cat - Cute Kalimba Lullaby  Background Music--cIiFcEhc7Y-192k-1635051532215"]
+        icon : "resources/img/icons/classical.svg",
+        label : "classical",
+        dir: "classical",
+        playlist : ["Kate's Waltz - Bad Snacks",
+                    "Drunken Sailor - Cooper Cannell",
+                    "Eine Kleine Nachtmusik (by Mozart) - Mozart",
+                    "Hndel-Xerxes-HWV-40",
+                    "Beethoven s Egmont (Overture), Op. 84 - LUDWIG VAN BEETHOVEN music classical_160k",
+                    "Lullaby - Cooper Cannell",
+                    "News Room News - Spence",
+                    "Ode to Joy - Cooper Cannell",
+                    "Mozart s The Magic Flute (Overture), K. 620 - WOLFGANG AMADEUS MOZART music classical_160k",
+                    "Star Spangled Banner - Cooper Cannel",
+                    "The Two Seasons - Dan Bodan"]
     },
 
     {
@@ -255,13 +280,28 @@ const musics = [
                     "The_Night_to_Romance_-_David_Fesliyan",
                     "Heat_Of_Spain_-_www.FesliyanStudios.com",
                     "Morning Mandolin - Chris Haugen",
+                    "Honolulu - Funny Upbeat Guitar & Kalimba Background Music-ubI_WdNH6DI-192k-1633838417110",
                     "Reaching_Out_-_www.FesliyanStudios.com_David_Renda",
                     "Riviera_Walk_-_www.FesliyanStudios.com",
+                    "_No Copyright Music Amber - Relaxing Acoustic Guitar Folk Background Music_160k",
                     "Spanish_Classical_Guitar_-_www.FesliyanStudios.com"]
     },
 
     {
         id : 6,
+        icon : "resources/img/icons/kalimba.svg",
+        label : "kalimba",
+        dir: "kalimba",
+        playlist : ["Oriente - Kalimba Lullaby  Background Music-xXe10UJvqKU-192k-1635171501921",
+                    "Honolulu - Funny Upbeat Guitar & Kalimba Background Music-ubI_WdNH6DI-192k-1633838417110",
+                    "_No Copyright Music Izzy s Lullaby - Cute Kalimba Background Music_160k",
+                    "Kalimba Lullaby  Meditation  Relaxation-QLGvrULY5oA-192k-1635172123551",
+                    "Sleepy Cat - Cute Kalimba Lullaby  Background Music--cIiFcEhc7Y-192k-1635051532215",
+                    "Free to use Talking to the moon x Play date kalimba cover (No copyright)_160k" ]
+    },
+
+    {
+        id : 7,
         icon : "resources/img/icons/cafe.svg",
         label : "cafe",
         dir: "cafe",
@@ -273,26 +313,15 @@ const musics = [
                     "cafe 1.6"]
     },
 
-    {
-        id : 7,
-        icon : "resources/img/icons/library.svg",
-        label : "library",
-        dir: "library",
-        playlist : ["Casa-Bossa-Nova.mp3",
-                    "CloserToJazz.mp3",
-                    "SidewaysSamba.mp3",
-                    "StandardJazzBars.mp3"]
-    },
 
     {
         id : 8,
-        icon : "resources/img/icons/city.svg",
-        label : "city",
-        dir: "city",
-        playlist : ["Casa-Bossa-Nova.mp3",
-                    "CloserToJazz.mp3",
-                    "SidewaysSamba.mp3",
-                    "StandardJazzBars.mp3"]
+        icon : "resources/img/icons/library.svg",
+        label : "library",
+        dir: "library",
+        playlist : ["LIBRARY SOUNDS NO COPYRIGHT AUDIO 2021 _160k",
+                    "library asmr",
+                    "writing and keyboard asmr"]
     },
 
     {
@@ -300,10 +329,17 @@ const musics = [
         icon : "resources/img/icons/nature.svg",
         label : "nature",
         dir: "nature",
-        playlist : ["Casa-Bossa-Nova.mp3",
-                    "CloserToJazz.mp3",
-                    "SidewaysSamba.mp3",
-                    "StandardJazzBars.mp3"]
+        playlist : ["ASMR - Water Sounds_160k",
+                    "Forest Sound Effect NO COPYRIGHTS_160k",
+                    "Rain Sounds No Copyright - Rain on a Quiet Forest Sounds Free Download No Copyrights RAIN ASMR_160k",
+                    "Nature Sounds Of Scotland Spring Time (Royalty Free Sound Effects)_160k",
+                    "FREE Ambient Sounds Of Snow Falling Sound Effect (Free Download)_160k",
+                    "Fire Crackling Sound Effect Royalty & Copyright Free_160k",
+                    "Rain Sounds No Copyright - Rain Sounds Under Umbrella Rain Sounds for Sleeping,Study RAIN ASMR_160k",
+                    "Relaxing Water Stream In Forest Nature Sound Effects_160k",
+                    "Strong Wind Sound Effect Royalty Free Sound Effects_160k",
+                    "Free Sound Effects - Water Sounds (Royalty free Sound Effects)_160k",
+                    "Rain Sounds No Copyright - The City Rain Sounds Rain Sounds No Thunder RAIN ASMR No Copyright_160k"]
     },
 
     {
@@ -311,10 +347,11 @@ const musics = [
         icon : "resources/img/icons/beach.svg",
         label : "beach",
         dir: "beach",
-        playlist : ["Casa-Bossa-Nova.mp3",
-                    "CloserToJazz.mp3",
-                    "SidewaysSamba.mp3",
-                    "StandardJazzBars.mp3"]
+        playlist : ["Calm Seashore - No Copyright Sound Effects - Audio Library_160k",
+                    "Ocean Waves Beach(Sound Effects)- SFX Producer (Vlog No Copyright Music)_160k",
+                    "sea waves [calm sound of waves in the sea] NO COPYRIGHT",
+                    "THE RELAXING SOUND OF THE BEACH WATER 4 ASMR_160k",
+                    "Sound Effect _ Beach ambience No Copyright Music (2021)_160k"]
     },
 
 
